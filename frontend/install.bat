3e697c14ac302bd141a7908a52b5fdf3d4bd8604
OmarELswaf <alsawaf.3mor@gmail.com>
2026-08-01 07:02:37 +0300

    chore: remove stray debug/build artifacts and ignore dist + tsbuildinfo

--- frontend/install.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set PATH=C:\Program Files\nodejs;%PATH%
-cd /d E:\project\HomeAssignment\frontend
-call "C:\Program Files\nodejs\npm.cmd" install 2>&1
-echo EXIT_CODE=%ERRORLEVEL%
--- frontend/run-tests.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set PATH=C:\PROGRA~1\nodejs;%PATH%
-cd /d E:\project\HomeAssignment\frontend
-"C:\PROGRA~1\nodejs\node.exe" ".\node_modules\vitest\vitest.mjs" run --reporter verbose
-echo EXIT_CODE=%ERRORLEVEL%
